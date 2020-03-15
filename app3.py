f33f81bca20ab251f1678e5782c2374bd703c4e4
Gaspero <gasp-gasp@mail.ru>
2020-03-15 20:56:36 +0300

    multi-channel support added

--- app3.py
+++ app3.py
@@ -3,6 +3,7 @@ from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 import logging
 import sys
+from telethon.tl.types import MessageEntityTextUrl, MessageEntityUrl, MessageEntityMention
 
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
@@ -23,12 +24,22 @@ client = TelegramClient(StringSession(SESSION_STRING), API_ID, API_HASH)
 channels = {1262282428: -1001469552065, 1272721495: -1001381191322}
 
 
+async def forbidden(event):
+    if event.message.entities:
+        for entity in event.message.entities:
+            if isinstance(entity, (MessageEntityTextUrl, MessageEntityUrl, MessageEntityMention)):
+                return True
+
 @client.on(events.NewMessage)
 async def my_event_handler(event):
     sender = await event.get_sender()
-    print(sender)
+    # print(sender)
     if sender.id in channels.keys():
-        await client.send_message(entity=channels[sender.id], message=event.message)
+        filtered = await forbidden(event)
+        if not filtered:
+            await client.send_message(entity=channels[sender.id], message=event.message)
+        else:
+            logging.info(f'Message from {sender.title} was not sent because of filter violation')
 
 client.start()
 logging.info('client started')
