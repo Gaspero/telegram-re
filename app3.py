8782e1fbb8d5bf22c2c91f214fe21144a6e04b9a
Gaspero <gasp-gasp@mail.ru>
2020-03-15 20:18:44 +0300

    multi-channel support added

--- app3.py
+++ app3.py
@@ -20,13 +20,15 @@ PORT = int(os.environ.get('PORT', '8000'))
 
 client = TelegramClient(StringSession(SESSION_STRING), API_ID, API_HASH)
 
+channels = {1262282428: -1001469552065, 1272721495: -1001381191322}
+
 
 @client.on(events.NewMessage)
 async def my_event_handler(event):
     sender = await event.get_sender()
     print(sender)
-    if sender.id == MASTER:
-        await client.send_message(entity=SLAVE, message=event.message)
+    if sender.id in channels.keys():
+        await client.send_message(entity=channels[sender.id], message=event.message)
 
 client.start()
 logging.info('client started')
